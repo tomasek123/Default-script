e19982bad246be837ea5887d494465a9c96dbb85
Tomas Malecek <t.malecek@gmail.com>
2023-03-10 23:33:42 +0100

    Lel

--- Fitting/measurement.py
+++ Fitting/measurement.py
@@ -4,6 +4,7 @@ from tkinter import *
 import pathlib
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Upozorneni !!!
 # Momentalni verze funguje pouze pro pole v x-ovem smeru !!!
@@ -23,7 +24,7 @@ class spectra:
     def __str__(self):
         return 'Real time      '+ str(self.RT) + '\n Repeat        ' + str(self.repeat)+ '\n Time          ' + str(self.time)+ '\n Angle         ' + str(self.angle)+ '\n Field         ' + str(self.field) + '\n Temperature   ' + str(self.temp)+ '\n Field current '+str(self.fc)
 
-class measurement:
+class measurement: #TODO pridej parameter sample name
     def __init__(self,spec,repeat,time,field,temp,findmin):
         self.list = spec      # List spekter v tomto mereni
         self.repeat = repeat
@@ -31,6 +32,11 @@ class measurement:
         self.field = field
         self.temp = temp
         self.findmin = findmin
+        self.fitted = 'Not defined'
+        self.fittable = 'Not defined'
+
+    def __str__(self):
+        return 'Repeat        ' + str(self.repeat)+ '\n Time         ' + str(self.time)+ '\n Findmin      ' + str(self.findmin)+ '\n Field        ' + str(self.field) + '\n Temperature  ' + str(self.temp)+ '\n Fittable     '+str(self.fittable)
 
     def get_spectra(self):
         # Pokud se meni uhel, pak nazev sloupcu jsou uhly
@@ -55,30 +61,45 @@ class measurement:
     
     def is_fitable(self):
         is_fit = True
+        self.fittable = True
         for meas in self.list:
             if meas.angle == 'Not defined':
                 is_fit = False
+                self.fittable = False
         if self.field == 'Not defined':
             is_fit = False
+            self.fittable = False
         if self.findmin:
             is_fit = False
+            self.fittable = False
         
         return is_fit
     
-    def fit(self): # todo dodelat
+    def fit(self): # TODO jak se pocita chyba ??
         if self.is_fitable():
-            lel = True
+            data = self.get_spectra()
+            angles = list(data.columns)
+            A = []
+            for angle in np.deg2rad(angles):
+                A.append([np.power(np.sin(angle),2),np.sin(2*angle),1])
+            A = np.linalg.pinv(A)
+            for ind,row in data.iterrows():
+                data.loc[ind,'Fit'] = A.dot(row.values)[1]/A.dot(row.values)[0]
+            data['eV'] = 1239.8/data.index.to_series()
+            data = data.set_index('eV')
         else:
-            lel = 'This is not a MOKE measurement!'
-        return lel
+            data = 'Not fittable'
 
-def LoadData():
+        self.fitted = data
+        return data
+
+def LoadData(): # TODO jeste podle pathy slozky budes muset separovat mereni... good luck
     # Load all txt files in chosen directory
     root = Tk()
     root.withdraw()
     root.call('wm', 'attributes', '.', '-topmost', True)
     initial_dir = os.getcwd()
-    initial_dir = r'C:\Users\tmale\Downloads\Test'  #todo delete
+    initial_dir = r'C:\Users\tmale\OneDrive\Documents\Data\LSMO\Francie 2021\MOKE\PLD3970\2023\spectra zrc'  #TODO delete
     folder = filedialog.askdirectory(initialdir = initial_dir,title = "Select measurement folders")
     root.destroy()
     Path_folder = pathlib.Path(folder)
@@ -121,11 +142,38 @@ def LoadData():
         measurements[i] = measurement(spectra_list[i],measurements[i][0],measurements[i][1],measurements[i][2],measurements[i][3],measurements[i][4])
  
     # Return a list of measurement class objects 
-    return measurements
+    return measurements, folder
 
 
-result = LoadData()
+def fitni(): # TODO
+    data,path = LoadData()
+    for i in range(0,len(data)):
+        data[i].fit()
+    splacnuto = []
+    done = []
+    for i in range(0,len(data)):
+        if data[i].fittable and i not in done:
+            splacnuto.append([data[i]])
+            for j in range(i+1,len(data)):
+                if data[i].repeat == data[j].repeat and data[i].time == data[j].time and data[i].temp == data[j].temp:
+                    splacnuto[-1].append(data[j])
+                    done.append(j)
 
-for i in result:
-    print(i.fit())
+    for mereni in splacnuto:
+        if len(mereni) == 1:
+            save_one_polarity()
+        elif len(mereni) == 2:
+            save_kerr()
+        else:
+            save_smycka()
+                
 
+fitni()
+
+# Funkce initial chces mit parametry  - save : default true (ukladej i plus/minus pole fity,sample name,jestli smycka nebo kerr, date, RT, repeat, temperature, time)
+
+# Funkce ukaz: - pm: default false
+#              - energie konkretni na smycku : default None
+
+# Funkce ukafit: ukaze fit na jedne energii
+#              - energie : default 4 eV
