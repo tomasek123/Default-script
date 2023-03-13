1a48293bdeca69b40154ce5f004de9344e267e8d
Tomas Malecek <t.malecek@gmail.com>
2023-03-13 12:45:50 +0100

    Measurement program save one polarity

--- Fitting/measurement.py
+++ Fitting/measurement.py
@@ -24,19 +24,21 @@ class spectra:
     def __str__(self):
         return 'Real time      '+ str(self.RT) + '\n Repeat        ' + str(self.repeat)+ '\n Time          ' + str(self.time)+ '\n Angle         ' + str(self.angle)+ '\n Field         ' + str(self.field) + '\n Temperature   ' + str(self.temp)+ '\n Field current '+str(self.fc)
 
-class measurement: #TODO pridej parameter sample name
-    def __init__(self,spec,repeat,time,field,temp,findmin):
+class measurement: #TODO pridej parameter sample name a measurement folder
+    def __init__(self,spec,repeat,time,field,temp,findmin,sample,folder_path):
         self.list = spec      # List spekter v tomto mereni
         self.repeat = repeat
         self.time = time
         self.field = field
         self.temp = temp
         self.findmin = findmin
+        self.sample = sample
+        self.folder = folder_path
         self.fitted = 'Not defined'
         self.fittable = 'Not defined'
 
     def __str__(self):
-        return 'Repeat        ' + str(self.repeat)+ '\n Time         ' + str(self.time)+ '\n Findmin      ' + str(self.findmin)+ '\n Field        ' + str(self.field) + '\n Temperature  ' + str(self.temp)+ '\n Fittable     '+str(self.fittable)
+        return '# Vzorek        ' + str(self.sample)+'\n# Repeat       ' + str(self.repeat)+ '\n# Time         ' + str(self.time)+ '\n# Findmin      ' + str(self.findmin)+ '\n# Field        ' + str(self.field) + '\n# Temperature  ' + str(self.temp)+ '\n# Fittable     '+str(self.fittable)+ '\n# Folder path  '+str(self.folder)
 
     def get_spectra(self):
         # Pokud se meni uhel, pak nazev sloupcu jsou uhly
@@ -55,7 +57,7 @@ class measurement: #TODO pridej parameter sample name
         data = data.sort_index()
         data = data.interpolate(method = 'index')
         data = data[~data.index.duplicated(keep='first')] # drop duplicate indexes
-        data = data[~data.isin([np.nan]).any(1)] 
+        data = data[~data.isin([np.nan]).any(axis=1)] 
 
         return data
     
@@ -93,7 +95,8 @@ class measurement: #TODO pridej parameter sample name
         self.fitted = data
         return data
 
-def LoadData(): # TODO jeste podle pathy slozky budes muset separovat mereni... good luck
+
+def LoadData(): # TODO jeste podle pathy slozky budes muset separovat mereni a podle jmena vzorku... good luck
     # Load all txt files in chosen directory
     root = Tk()
     root.withdraw()
@@ -114,6 +117,7 @@ def LoadData(): # TODO jeste podle pathy slozky budes muset separovat mereni... 
     # and load it into the spectra class
     for file in files:
         name = str(file).rsplit('\\',1)[1]
+        path = str(file).rsplit('\\',1)[0]
         data = pd.read_csv(file, comment='#', index_col=0, sep='\t', header=None)
         RT = name[0:18]
         name = name.split('_')
@@ -130,7 +134,7 @@ def LoadData(): # TODO jeste podle pathy slozky budes muset separovat mereni... 
             findmin = True
         else:
             findmin = False
-        meas = [properties[0],properties[1],properties[3],properties[4],findmin]
+        meas = [properties[0],properties[1],properties[3],properties[4],findmin,name[2],path]
         if meas in measurements:
             indexx = measurements.index(meas)
             spectra_list[indexx].append(spectra(data,file,RT,properties[0],properties[1],properties[2],properties[3],properties[4],properties[5]))
@@ -139,13 +143,12 @@ def LoadData(): # TODO jeste podle pathy slozky budes muset separovat mereni... 
             spectra_list.append([spectra(data,file,RT,properties[0],properties[1],properties[2],properties[3],properties[4],properties[5])])
 
     for i in range(0,len(measurements)):
-        measurements[i] = measurement(spectra_list[i],measurements[i][0],measurements[i][1],measurements[i][2],measurements[i][3],measurements[i][4])
+        measurements[i] = measurement(spectra_list[i],measurements[i][0],measurements[i][1],measurements[i][2],measurements[i][3],measurements[i][4],measurements[i][5],measurements[i][6])
  
-    # Return a list of measurement class objects 
+    # Return a list of measurement class objects and opened folder path
     return measurements, folder
 
-
-def fitni(): # TODO
+def fitni():
     data,path = LoadData()
     for i in range(0,len(data)):
         data[i].fit()
@@ -161,19 +164,51 @@ def fitni(): # TODO
 
     for mereni in splacnuto:
         if len(mereni) == 1:
-            save_one_polarity()
+            save_one_polarity(mereni,path)
         elif len(mereni) == 2:
-            save_kerr()
+            save_kerr(mereni,path)
         else:
-            save_smycka()
-                
+            save_smycka(mereni,path)
 
-fitni()
+def save_one_polarity(data,path):  # TODO
+    if data[0].temp != 'Not defined':
+        path = path + data[0].temp
+    if data[0].repeat != 'Not defined':
+        path = path + data[0].repeat
+    if data[0].time != 'Not defined':
+        path = path + data[0].time
+    if data[0].list[0].field != 'Not defined':
+        path = path + data[0].time
+    file = open(path + '\\' + data[0].sample + data[0].repeat + data[0].temp + data[0].time +'.dat','a')
+    if data[0].list[0].field != 'Not defined':
+        file.write('# Type of measurement: One polarity of field at '+ data[0].list[0].field + 'T' )
+    else:
+        file.write('# Type of measurement: One polarity of field at ??T')
+    file.write('# Date measured: ' + data[0].list[0].RT[-1:-3] + '.' + data[0].list[0].RT[-3:-5]+'.'+data[0].list[0].RT[:4])
+    file.write(str(data[0]))
 
-# Funkce initial chces mit parametry  - save : default true (ukladej i plus/minus pole fity,sample name,jestli smycka nebo kerr, date, RT, repeat, temperature, time)
+def save_kerr(data,path):# TODO
+    print('lol')
+
+def save_smycka(data,path):# TODO
+    print('lol')
+
+
+
+
+# Funkce fitni (ukladej i plus/minus pole fity,sample name,jestli smycka nebo kerr, date, RT, repeat, temperature, time,folder)
 
 # Funkce ukaz: - pm: default false
 #              - energie konkretni na smycku : default None
 
-# Funkce ukafit: ukaze fit na jedne energii
+# Funkce ukafit: ukaze fit na jedne energii 
 #              - energie : default 4 eV
+#              - field : default 1
+#   - Bude fungovat tak, ze si z nafitovaneho souboru precte path a parametry
+#     a nasledne je nalezne ve slozce (snad) a nafituje na te konkretni energii
+#
+# Funkce elip: spocte elipticitu a vytvori jeden soubor rotace a elipticity
+#              - soubor bude obsahovat data kdy bylo co zmerene, jmeno vzorku
+#
+# Funkce savitzky: proste filtr
+#                 - parameter save : default no
